bf9a21b0b0e47406e71d34c7dda50ba690d9597e
stomobe <55563963+stomobe@users.noreply.github.com>
2024-11-23 21:55:47 -0500

    Updated orb
    Displays score gained when orb collected

--- orb.gd
+++ orb.gd
@@ -10,7 +10,7 @@ var orb_score_value = 20
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$SpawnSound.play()
-	$AnimatedSprite2D.play()
+	$AnimatedSprite2D.play("default")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,7 +20,14 @@ func _process(_delta: float) -> void:
 	
 func orb_collected_sequence():
 	$PickupSound.play()
+	$AnimatedSprite2D.play("collected")
+	
 	GameState.score += orb_score_value
+	
+	$PlusScore.position = $AnimatedSprite2D.position
+	$PlusScore.visible = true
+	await get_tree().create_timer(0.6).timeout
+	$PlusScore.visible = false
 
 
 func _on_pickup_sound_finished() -> void:
